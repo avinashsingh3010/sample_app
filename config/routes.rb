0a3b1d6a2bb1009b5f40d263b9b87cd96efdf9b0
avinashsingh3010 <singh.avinash307@gmail.com>
2016-07-18 15:06:55 +0530

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 SampleApp::Application.routes.draw do
 
 
+  get 'users/new'
+
+  	get '/signup', :to => 'users#new'
 	get '/contact', :to => 'pages#contact' 
 	get '/about', :to => 'pages#about'
 	get '/help', :to => 'pages#help'
